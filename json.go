bcf1b067ce5debfc82d0849fa21ea98ecee798a6
Jerome de Dios <jeromededios.de@proton.me>
2025-09-17 22:19:12 +0200

    fixed gosec

--- json.go
+++ json.go
@@ -31,12 +31,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 
+	w.WriteHeader(code)
 	_, err = w.Write(dat)
 
 	if err != nil {
 		log.Printf("failed to write response: %v", err)
 	}
-
-	w.WriteHeader(code)
-	w.Write(dat)
 }
